428138ec6234830ba52d3645fdaf1206efd70955
ckelly2112 <ckelly28@myseneca.ca>
2019-12-23 09:17:02 -0500

    Setup app.js and first route

---
+++ app.js
@@ -0,0 +1,40 @@
+const PORT = process.env.PORT || 3000;
+
+const express = require("express")
+const exphbr = require("express-handlebars")
+const bodyParser = require("body-parser")
+const session = require("express-session")
+
+//Routes
+
+//Start Express
+const app = express();
+
+//Processes
+require("dotenv").config({path:'./config/keys.env'});
+
+//Handlebars
+app.engine('handlebars', exphbr());
+app.set('view engine', 'handlebars');
+
+//Public Folder
+app.use(express.static('public'));
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Sessions
+app.use(session({secret:process.env.SESSION_SECRET}));
+app.use((req,res,next)=>{
+    res.locals.user= req.session.userInfo;
+    next();
+})
+
+//Routes go here
+
+
+
+//Start
+app.listen(PORT, ()=>{
+    console.log(`Now listening on port: ${PORT}`)
+})
