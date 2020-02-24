78596309e7d7953c5d3b7e849c84c51f97259c69
ckelly2112 <ckelly28@myseneca.ca>
2020-02-24 13:29:54 -0500

    All Rush web functionality working

--- routes/web.js
+++ routes/web.js
@@ -12,4 +12,9 @@ route.get('/Template', (req, res)=>{
     res.redirect('https://horizon-demo.squarespace.com/?nochrome=false')
 })
 
+route.get('/RushWebsite', (req,res)=>{
+    res.sendFile('index.html', {root: './public/html'})
+})
+
+
 module.exports=route;
