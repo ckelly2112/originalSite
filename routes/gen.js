363598fc411585e0ed42ab8333b6b31049e6a184
ckelly2112 <ckelly28@myseneca.ca>
2020-02-25 16:44:52 -0500

    Lots of changes so far

--- routes/gen.js
+++ routes/gen.js
@@ -5,4 +5,11 @@ route.get('/', (req,res)=>{
     res.render('general/home');
 })
 
+route.get('/aboutMe', (req,res)=>{
+    res.render('general/aboutMe')
+})
+
+route.get('/contactMe', (req,res)=>{
+    res.render('general/contactMe')
+})
 module.exports = route;
